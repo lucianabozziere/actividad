68cb11e5e8fa5ba49e9cf85c67e2c9a1a217b06f
lucianabozziere <141955692+lucianabozziere@users.noreply.github.com>
2023-08-10 12:40:03 -0600

    Add files via upload

---
+++ hola mundo.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+   printf("Generaciones de computadoras \n\n");
+   printf("Primera generacion-1940-1956\n");
+   printf("Caracteristicas: Utilizacion de valvulas de vacio para procesamiento y memoria, programacion mediante cables y tableros, tamaño grande, alto consumo de energia, programacion a bajo nivel.\n");
+   printf("Ejemplos: ENIAC, UNIVAC I, EDSAC \n\n");
+   
+   printf("Segunda generacion-1956-1963 \n");
+   printf("Caracteristicas: Uso de transistores en lugar de valvulas de vacio, menor tamaño, mayor velocidad y eficiencia, introduccion de lenguajes de programacion de alto nivel, almacenamiento en cintas magneticas y discos.\n");
+   printf("Ejemplos: IBM 7090, UNIVAC II, DEC PDP-1.\n\n");
+   
+   printf("Tercera generacion-1964-1971\n");
+   printf("Caracteristicas: Introduccion de circuitos integrados (chips), mayor capacidad y velocidad, sistemas multiprogramacion y multitarea, sistemas operativos mas avanzados, lenguajes de programacion mas potentes\n");
+   printf("Ejemplos: IBM System/360, DEC PDP-8, CDC 6600 \n\n");
+   
+   printf("Cuarta generacion-1971-1980\n");
+   printf("Caracteristicas: Microprocesadores (chips con CPU en un solo componente), miniaturizacion, computadoras personales y estaciones de trabajo, interfaces graficas de usuario, nacimiento de la industria de la computadora personal\n");
+   printf("Ejemplos: Intel 4004, IBM System/370, Apple II, IBM PC \n\n");
+   
+   printf("Quinta generacion-desde 1980 en adelante\n");
+   printf("Avances en microprocesadores y chips, con mejoras continuas en velocidad y capacidad. Desarrollo de tecnologias de redes y comunicacion. Auge de la computacion personal y empresarial. Desarrollo de dispositivos moviles y la era de Internet\n");
+   return 0;
+}
